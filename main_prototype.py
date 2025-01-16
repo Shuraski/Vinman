3b686f0a63be539e3fe4c42cdee48edd6d3e064f
Shuraski <sashanik9623@gmail.com>
2025-01-16 13:35:18 -0600

    pivot to integrated terminal ui

--- main_prototype.py
+++ main_prototype.py
@@ -1,26 +1,9 @@
-import argparse
-from src.modules import ModelUtils
+from src.chat import ModelUtils
 
-def main(model_name):
+def loadmodel(model_name):
     model_utils = ModelUtils(model_name)
     model_utils.check_model_installed()
     ModelUtils.save_chosen_model(model_name)
     chosen_model = ModelUtils.load_chosen_model()
-    print(chosen_model)
 
-def cli():
-    parser = argparse.ArgumentParser(description="Model Utility CLI")
-    subparsers = parser.add_subparsers(dest='command')
-
-    run_parser = subparsers.add_parser('run', help='Run a model')
-    run_parser.add_argument('model_name', type=str, nargs='?', default='llama3.2', help='Name of the model to use')
-
-    args = parser.parse_args()
-
-    if args.command == 'run':
-        main(args.model_name)
-    else:
-        parser.print_help()
-
-if __name__ == "__main__":
-    cli()
+loadmodel('')
