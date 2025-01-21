62fc98deb68ef3ab7474b889fbe4f31692e5a107
Shuraski <sashanik9623@gmail.com>
2025-01-20 22:48:10 -0600

    update print statements

--- main_prototype.py
+++ main_prototype.py
@@ -6,9 +6,9 @@ itu = ITU_MainMenu()
 itu_result = itu.run()
 
 if itu_result == 1:
-    print('This feature is being refactored')
+    print('This feature is currently being refactored')
 elif itu_result == 2:
     cig = CustomInstructionGenerator(chosen_model='llama3.2')
     cig._interactive_instruction_builder()
 elif itu_result == 3:
-    print('Double Texting feature is currently unstable')
+    print('This feature is currently being refactored')
